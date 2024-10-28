f620e47e0fa65bf68bf3e17017000e300283c124
Subzerozdev <khanhtrantuc2004@gmail.com>
2024-10-28 08:54:27 +0700

    Add new

---
+++ AirConditionerShop.DAL/Models/AirConditionerShop2024DbContext.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace AirConditionerShop.DAL.Models;
+
+public partial class AirConditionerShop2024DbContext : DbContext
+{
+    public AirConditionerShop2024DbContext()
+    {
+    }
+
+    public AirConditionerShop2024DbContext(DbContextOptions<AirConditionerShop2024DbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<AirConditioner> AirConditioners { get; set; }
+
+    public virtual DbSet<StaffMember> StaffMembers { get; set; }
+
+    public virtual DbSet<SupplierCompany> SupplierCompanies { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        // pas hàm dưới vào đây nhé 
+        => optionsBuilder.UseSqlServer(GetConnectionString());
+
+
+    // đây là hàm mới nè copy từ file note nhé
+    private string GetConnectionString()
+    {
+        IConfiguration config = new ConfigurationBuilder()
+             .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", true, true)
+                    .Build();
+        var strConn = config["ConnectionStrings:DefaultConnectionStringDB"];  // coi đây có dấu cách gì không á // so sánh appsettings với đoạn này 
+        //                       Dấu cách và 9 triệu ( 6 triệu + 3 triệu) 
+
+        return strConn;
+    }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<AirConditioner>(entity =>
+        {
+            entity.HasKey(e => e.AirConditionerId).HasName("PK__AirCondi__EE2EB739C554735B");
+
+            entity.ToTable("AirConditioner");
+
+            entity.Property(e => e.AirConditionerId).ValueGeneratedNever();
+            entity.Property(e => e.AirConditionerName).HasMaxLength(200);
+            entity.Property(e => e.FeatureFunction).HasMaxLength(250);
+            entity.Property(e => e.SoundPressureLevel).HasMaxLength(80);
+            entity.Property(e => e.SupplierId).HasMaxLength(20);
+            entity.Property(e => e.Warranty).HasMaxLength(60);
+
+            entity.HasOne(d => d.Supplier).WithMany(p => p.AirConditioners)
+                .HasForeignKey(d => d.SupplierId)
+                .OnDelete(DeleteBehavior.Cascade)
+                .HasConstraintName("FK__AirCondit__Suppl__3C69FB99");
+        });
+
+        modelBuilder.Entity<StaffMember>(entity =>
+        {
+            entity.HasKey(e => e.MemberId).HasName("PK__StaffMem__0CF04B38C0F11B39");
+
+            entity.ToTable("StaffMember");
+
+            entity.HasIndex(e => e.EmailAddress, "UQ__StaffMem__49A1474021C5A641").IsUnique();
+
+            entity.Property(e => e.MemberId)
+                .ValueGeneratedNever()
+                .HasColumnName("MemberID");
+            entity.Property(e => e.EmailAddress).HasMaxLength(60);
+            entity.Property(e => e.FullName).HasMaxLength(60);
+            entity.Property(e => e.Password).HasMaxLength(40);
+        });
+
+        modelBuilder.Entity<SupplierCompany>(entity =>
+        {
+            entity.HasKey(e => e.SupplierId).HasName("PK__Supplier__4BE666B43676229B");
+
+            entity.ToTable("SupplierCompany");
+
+            entity.Property(e => e.SupplierId).HasMaxLength(20);
+            entity.Property(e => e.PlaceOfOrigin).HasMaxLength(60);
+            entity.Property(e => e.SupplierDescription).HasMaxLength(250);
+            entity.Property(e => e.SupplierName).HasMaxLength(80);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ AirConditionerShop_NguyenGiaKhanh/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AirConditionerShop_NguyenGiaKhanh
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    // CLASS MAINWINDOW KẾ THỪA CLASS WINDOW - CLASS CÓ SẴN TRONG DOTNET SDK
+    // CLASS MAINWINDOW NÀY CÓ 2 KHẢ NĂNG:
+    // OOP NHƯ BÌNH THƯỜNG
+    // RENDER RA MÀN HÌNH NHƯ MỌI CỬA SỔ CỦA CÁC APP KHÁC
+    // THÊM KHẢ NĂNG THỨ 3: 
+    // VÌ CÓ PHẦN RENDER NÊN TA CÓ THỂ THAY ĐỔI CÁC PROPERTY CỦA CÁI CỬA SỔ THÔNG QUA CODE HOẶC QUA MÀN HÌNH DESIGN KÉO THẢ CHUỘT,
+    // HOẶC QUA PHẦN GÕ TAG THEO STYLE XAML - GIỐNG HTML/CSS - TẠM GỌI HTML/CSS KIỂU MICROSOFT
+    // NHƯNG TAG NÀU CHỈ RENDER RA UI CỬA SỔ CHẠY TRÊN DESKTOP, CHẠY TRÊN BROWSER????
+
+    // ĐỂ CHUYENR QUA LẠI GIỮA 2 MODE: CODE OOP VÀ DESIGN --> DÙNG PHÍM F7 TỪ DESIGN --> CODE
+    //                                                       PHÍM SHIFT F7 TỪ CODE --> DESIGN
+    // PHẦN OOP - FILE .XAML.CS --> ĐƯỢC GỌI LÀ CODE BEHIND --. CODE NẰM SAU TRANG/ RENDER UI
+
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
